49cfdb2c3f4e6ff7bc22f8d4ffded2b7cdca0506
git-cst <csteenberg@hotmail.co.uk>
2024-12-06 14:55:41 +0100

    Complete day 5.

--- day_5/day5.py
+++ day_5/day5.py
@@ -27,34 +27,54 @@ def generate_rule_set() -> {list[str]}:
     
     return complete_rule_set
 
-def valid(rule_set: dict, input: str) -> bool:
+def valid(rule_set: dict, input: list[str]) -> bool:
     i = 0
     while i < len(input) - 1:
         key = input[i]
-        for j in range(i + 1, len(input) - 1):
+        for j in range(i + 1, len(input)):
             val_to_check = input[j]
             if val_to_check not in rule_set[key]:
                 return False
         i += 1
-    
     return True
 
+def reorder(rule_set: dict, input: list[str]) -> list[str]:
+    if valid(rule_set, input):
+        return input
+
+    array = input[:]
+    
+    i = 0
+    while i < len(array) - 1:
+        key = array[i]
+        next_val = array[i + 1]
+        
+        if next_val not in rule_set.get(key, []):
+            array[i], array[i + 1] = array[i + 1], array[i]
+            i = 0
+            continue
+        
+        i += 1
+    return array if valid(rule_set, array) else [0]
+
 def median_val(valid_input: list[str]) -> int:
-    median = len(valid_input) // 2
-    median_val = valid_input[median]
-    return int(valid_input[median])
+    return int(valid_input[len(valid_input) // 2])
  
 def solve():
     data = generate_data_to_check()
     rule_set = generate_rule_set()
 
-    result = 0
+    valid_results = 0
+    reordered_results = 0
     for row in data:
         row_as_list = row.split(',')
         if valid(rule_set, row_as_list):
-            result += median_val(row_as_list)
+            valid_results += median_val(row_as_list)
+        else:
+            reordered_row = reorder(rule_set, row_as_list)
+            reordered_results += median_val(reordered_row)
 
-    print(result)
+    print(reordered_results)
 
 if __name__ == '__main__':
     solve()
