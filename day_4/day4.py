0a300909cf130916dd57a795f077118006e7275c
csteenberg <csteenberg@hotmail.co.uk>
2024-12-06 19:09:33 +0100

    Update day 4

--- day_4/day4.py
+++ day_4/day4.py
@@ -1,14 +1,16 @@
 import os
 
 def get_data():
-    with open(f'{os.path.dirname(__file__)}/day3_input.txt', 'r') as file:
+    with open(f'{os.path.dirname(__file__)}/day4_input.txt', 'r') as file:
         data: str = file.read()
     
+    data = data.split()
+
     return data
 
 class Cell():
     def __init__(self, value = None):
-        self.value = value
+        self.value: str = value
         
         self.n = None
         self.s = None
@@ -30,4 +32,39 @@ class Graph():
 
         for i in range(0, self.num_cols):
             for j in range(0, self.num_rows):
-                self.cells.value = data[i][j]
+                self.cells[i][j].value = data[i][j]
+
+                # GENERATE THE ADJACENCIES
+                # LOGIC FOR CHECKING NE
+
+                # LOGIC FOR CHECKING N
+
+                # LOGIC FOR CHECKING NW
+
+                # LOGIC FOR CHECKING W
+
+                # LOGIC FOR CHECKING E
+
+                # LOGIC FOR CHECKING SW
+
+                # LOGIC FOR CHECKING S
+
+                # LOGIC FOR CHECKING SE
+
+def check_adjacencies(graph: Graph):
+    count_of_xmas = 0
+
+    for i in range(0, graph.num_cols):
+        for j in range(0, graph.num_rows):
+            pass
+
+    return count_of_xmas
+
+def solve():
+    data = get_data()
+    graph = Graph(len(data), len(data[0]))
+    graph.generate_graph(data)
+    print(check_adjacencies(graph))
+
+if __name__ == "__main__":
+    solve()
