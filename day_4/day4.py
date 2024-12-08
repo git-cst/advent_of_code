34cb70e6aa47f79518d12e9c185a93e9f3e34147
csteenberg <csteenberg@hotmail.co.uk>
2024-12-08 09:59:52 +0100

    Add comments to explain

--- day_4/day4.py
+++ day_4/day4.py
@@ -45,71 +45,62 @@ class Graph():
                 cell: Cell = self.cells[i][j]
                 cell.value = data[i][j]
 
-                # LOGIC FOR CHECKING NW
+                # Logic for checking NW
                 if i - 1 >= 0 and j - 1 >= 0:
                     cell.nw = self.cells[i - 1][j - 1]
 
-                # LOGIC FOR CHECKING N
+                # Logic for checking N
                 if i - 1 >= 0:
                     cell.n = self.cells[i - 1][j]
 
-                # LOGIC FOR CHECKING NE
+                # Logic for checking NE
                 if i - 1 >= 0 and j + 1 < self.num_cols:
                     cell.ne = self.cells[i - 1][j + 1]
 
-                # LOGIC FOR CHECKING E
+                # Logic for checking E
                 if j + 1 < self.num_cols:
                     cell.e = self.cells[i][j + 1]
 
-                # LOGIC FOR CHECKING SE
+                # Logic for checking SE
                 if i + 1 < self.num_rows and j + 1 < self.num_cols:
                     cell.se = self.cells[i + 1][j + 1]
 
-                # LOGIC FOR CHECKING S
+                # Logic for checking S
                 if i + 1 < self.num_rows:
                     cell.s = self.cells[i + 1][j]
 
-                # LOGIC FOR CHECKING SW
+                # Logic for checking SW
                 if i + 1 < self.num_rows and j - 1 >= 0:
                     cell.sw = self.cells[i + 1][j - 1]
 
-                # LOGIC FOR CHECKING W
+                # Logic for checking W
                 if j - 1 >= 0:
                     cell.w = self.cells[i][j - 1]
-
-    def check_adjacencies(self):
-        count_of_xmas = 0
-        count_of_mas_in_x_form = 0
-
-        for i in range(0, self.num_rows):
-            for j in range(0, self.num_cols):
-                if self.cells[i][j].value == 'X':
-                    count_of_xmas += self.look_for_xmas(i, j)
-
-                if self.cells[i][j].value == 'A':
-                    count_of_mas_in_x_form += self.look_for_mas_in_x_form(i, j)
-
-        return count_of_xmas, count_of_mas_in_x_form
 
     def look_for_xmas(self, i, j):
         count = 0
         start_cell = self.cells[i][j]
         directions_to_search = ['nw', 'n', 'ne', 'e', 'se', 's', 'sw', 'w']
 
+        # Loop through directions from a center point that is X
         for direction in directions_to_search:
             value = None
             index = 0
             chars_to_find = ['M', 'A', 'S']
             cell = getattr(start_cell, direction, None)
 
+            # If there is a cell in that direction get the value of that cell
             if cell:
                 value = getattr(cell, "value", None)
 
+            # Loop through the index and check if each letter is the next expected value
             while value == chars_to_find[index]:
+                # If the value found is S exit and increment count and then check next direction from start cell
                 if value == 'S':
                     count += 1
                     break
 
+                # If value is correct go to next cell and get it's value
                 cell = getattr(cell, direction, None)
                 value = getattr(cell, "value", None)
                 index += 1
@@ -126,6 +117,7 @@ class Graph():
         ne_cell: Cell = getattr(start_cell, 'ne', None)
         sw_cell: Cell = getattr(start_cell, 'sw', None)
 
+        # If any cell is missing then MAS in X form cannot be found
         if not nw_cell or not se_cell or not ne_cell or not sw_cell:
             return count
 
@@ -148,7 +140,22 @@ class Graph():
             count += 1
 
         return count 
+    
+    def check_adjacencies(self):
+        count_of_xmas = 0
+        count_of_mas_in_x_form = 0
 
+        # Check each cell in graph if a value is X then it is possible to find XMAS if valus is A then it is possible to find MAS in X form
+        for i in range(0, self.num_rows):
+            for j in range(0, self.num_cols):
+                if self.cells[i][j].value == 'X':
+                    count_of_xmas += self.look_for_xmas(i, j)
+
+                if self.cells[i][j].value == 'A':
+                    count_of_mas_in_x_form += self.look_for_mas_in_x_form(i, j)
+
+        return count_of_xmas, count_of_mas_in_x_form
+    
 @time_execution
 def solve():
     data = get_data()
