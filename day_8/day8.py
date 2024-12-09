c3d69dfa66bed8688ea039a9817b82a82715981e
csteenberg <csteenberg@hotmail.co.uk>
2024-12-09 18:55:56 +0100

    commit

--- day_8/day8.py
+++ day_8/day8.py
@@ -12,39 +12,83 @@ def time_execution(func):
 
 def get_data():
     data = {}
-    with open(f'{os.path.dirname(__file__)}/day7_input.txt', 'r') as file:
+    with open(f'{os.path.dirname(__file__)}/day8_input.txt', 'r') as file:
         file_data: str = file.read()
     
     file_data = file_data.split('\n')
-    return data
+    return file_data
 
-def generate_node_pairs(data):
+def generate_node_coords(data):
     node_pair_dictionary = {}
-    hashmap_of_populated_cells = {}
-    # create a set of all nodes e.g. there is a 3 node, a a node, a X node, etc.
-    # create all the pairings of these nodes as a dictionary containing tuples which are coordinate sets?
+    populated_cells = {}
+
+    # Loop through the entire grid
+    # Generate a hashmap of occupied cells used for anti-node look up to see if it can be placed
+    # Generate a hashmap of nodes: Where each key contains all the node coordinates
+    for i in range(0, len(data)):
+        for j in range(0, len(data[0])):
+            if data[i][j] != '.':
+                populated_cells[(i, j)] = 1
+                if data[i][j] in node_pair_dictionary:
+                    temp: list[tuple] = node_pair_dictionary[data[i][j]]
+                    temp.append((i,j))
+                    node_pair_dictionary[data[i][j]] = temp
+                else:
+                    node_pair_dictionary[data[i][j]] = [(i, j)]
     
-    return node_pair_dictionary, hashmap_of_populated_cells
+    return node_pair_dictionary, populated_cells
 
-def generate_anti_nodes(node_pair_dictionary: dict, hashmap_of_populated_cells: dict):
-    unique_anti_nodes = 0
-    
-    for key in node_pair_dictionary.keys():
-        for coordinate_set in node_pair_dictionary[key]:
-            pass
-            # generate anti nodes
-                # anti node positions can be described as the distance between each node and just - the first node and + on the second node.
-                # check if the created coordinates would result in a negative value (as such it would be invalid and go to next anti node creation)
-            # check if anti node would already be in already populated cells
-                # if yes then don't increment unique anti nodes
-                # if no then increment unique anti nodes and add the coordinates to the hashmap of populated cells
+def calculate_antinodes_recursive(node_coords, populated_cells, current_index=0, antinodes=None) -> list:
+    if antinodes is None:
+        antinodes = []
 
-    # DO STUFF
+    # Base case: If we reach the last node, stop recursion
+    if current_index >= len(node_coords) - 1:
+        return antinodes
 
-    return unique_anti_nodes
+    # Get the current base node
+    base_node = node_coords[current_index]
+
+    # Recurse through remaining nodes
+    for i in range(current_index + 1, len(node_coords)):
+        # Pair the base node with another node
+        other_node = node_coords[i]
+        x1, y1 = base_node
+        x2, y2 = other_node
+
+        # Compute the displacement vector
+        delta_x = x2 - x1
+        delta_y = y2 - y1
+
+        # Calculate the antinode coordinates
+        antinode1 = (x1 - delta_x // 3, y1 - delta_y // 3)
+        # Check if the coordinates are out of bounds and the antinode does not already exist
+        if (antinode1[0] >= 0 and antinode1[1] >= 0) and antinode1 not in populated_cells:
+            antinodes.append(antinode1)
+            populated_cells[antinode1] = 1
+
+        # Repeat for antinode 2
+        antinode2 = (x2 - delta_x // 3, y2 - delta_y // 3)
+        if (antinode2[0] >= 0 or antinode2[1] >= 0) and antinode2 not in populated_cells:
+            antinodes.append(antinode2)
+            populated_cells[antinode2] = 1
+
+    # Recurse with the next base node
+    return calculate_antinodes_recursive(node_coords, populated_cells, current_index + 1, antinodes)
 
 def solve():
-    pass
+    data = get_data()
+    node_pair_dictionary, populated_cells = generate_node_coords(data)
+
+    antinodes = []
+    for key in node_pair_dictionary.keys():
+        antinodes.append(calculate_antinodes_recursive(node_pair_dictionary[key], populated_cells))
+
+    count_unique_antinodes = 0
+    for set_of_antinodes in antinodes:
+        count_unique_antinodes += len(set_of_antinodes)
+
+    print(count_unique_antinodes)
 
 if __name__ == "__main__":
     solve()
