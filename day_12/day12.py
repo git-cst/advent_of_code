d8a6787ca039965ebafecef586ba4f4cacf90410
git-cst <csteenberg@hotmail.co.uk>
2024-12-17 16:01:07 +0100

    Work on day 12 part 2

--- day_12/day12.py
+++ day_12/day12.py
@@ -1,5 +1,12 @@
 import os; import time
 
+"""J ikke rigtig
+E ikke rigtig
+F ikke rigtig
+C region_0 ikke rigtig
+I region 1 ikke rigtig
+"""
+
 def time_execution(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
@@ -11,7 +18,7 @@ def time_execution(func):
     return wrapper
 
 def get_data():
-    with open(f'{os.path.dirname(__file__)}/day12_testinput.txt', 'r') as file:
+    with open(f'{os.path.dirname(__file__)}/day12_testinput5.txt', 'r') as file:
         file_data: str = file.read()
     
     file_data = file_data.split('\n')
@@ -21,7 +28,170 @@ def get_data():
         array.append(value)
 
     return array
-      
+
+def num_boundary(outside_region):
+    corner_count = 0
+    match outside_region:
+        # Top-left corner (both inner and outer possible)
+        case [False, False, False, False, True, False, False, False]:  # Inner corner (variation 1)
+            print('Top left inner corner variation 1')
+            corner_count += 1
+        case [False, False, False, True, True, False, False, False]:  # Inner corner (variation 2)
+            print('Top left inner corner variation 2')
+            corner_count += 1
+        case [False, False, False, False, True, True, False, False]:  # Inner corner (variation 3)
+            print('Top left inner corner variation 3')
+            corner_count += 1
+        case [True, True, True, False, False, False, True, True]:  # Outer corner
+            print('Top left outer corner')
+            corner_count += 1
+        case [True, True, True, False, True, False, True, True]:  # Inner + Outer (variation 1)
+            print('Top left inner outer corner variation 1')
+            corner_count += 2
+        case [True, True, False, False, True, False, True, True]:  # Inner + Outer (variation 2)
+            print('Top left inner outer corner variation 2')
+            corner_count += 2
+
+        # Top-right corner (both inner and outer possible)
+        case [False, False, False, False, False, False, True, False]:  # Inner corner (variation 1)
+            print('Top right inner corner variation 1')
+            corner_count += 1
+        case [False, False, False, False, False, False, True, True]:  # Inner corner (variation 2)
+            print('Top right inner corner variation 2')
+            corner_count += 1
+        case [False, False, False, False, False, True, True, False]:  # Inner corner (variation 3)
+            print('Top right inner corner variation 3')
+            corner_count += 1
+        case [True, True, True, True, True, False, False, False]:  # Outer corner
+            print('Top right outer corner')
+            corner_count += 1
+        case [True, True, True, True, True, False, True, False]:  # Inner + Outer (variation 1)
+            print('Top right inner outer corner variation 1')
+            corner_count += 2
+        case [False, True, True, True, True, False, True, False]:  # Inner + Outer (variation 2)
+            print('Top right inner outer corner variation 2')
+            corner_count += 2
+
+        # Bottom-right corner (both inner and outer possible)
+        case [True, False, False, False, False, False, False, False]:  # Inner corner (variation 1)
+            print('Bottom right inner corner variation 1')
+            corner_count += 1
+        case [True, False, False, False, False, False, False, True]:  # Inner corner (variation 2)
+            print('Bottom right inner corner variation 2')
+            corner_count += 1
+        case [True, True, False, False, False, False, False, False]:  # Inner corner (variation 3)
+            print('Bottom right inner corner variation 3')
+            corner_count += 1
+        case [False, False, True, True, True, True, True, False]:  # Outer corner
+            print('Bottom right outer corner')
+            corner_count += 1
+        case [True, False, True, True, True, True, True, False]:  # Inner + Outer (variation 1)
+            print('Bottom right inner outer corner variation 1')
+            corner_count += 2
+        case [True, False, True, True, True, True, True, False]:  # Inner + Outer (variation 2)
+            print('Bottom right inner outer corner variation 2')
+            corner_count += 2
+
+        # Bottom-left corner (both inner and outer possible)
+        case [False, False, True, False, False, False, False, False]:  # Inner corner (variation 1)
+            print('Bottom left inner corner variation 1')
+            corner_count += 1
+        case [False, False, True, True, False, False, False, False]:  # Inner corner (variation 2)
+            print('Bottom left inner corner variation 2')
+            corner_count += 1
+        case [False, True, True, False, False, False, False, False]:  # Inner corner (variation 3)
+            print('Bottom left inner corner variation 2')
+            corner_count += 1
+        case [True, False, False, False, True, True, True, True]:  # Outer corner
+            print('Bottom left outer corner')
+            corner_count += 1
+        case [True, False, True, False, True, True, True, True]:  # Inner + Outer (variation 1)
+            print('Bottom left inner outer corner variation 1')
+            corner_count += 2
+        case [True, False, True, False, False, True, True, True]:  # Inner + Outer (variation 2)
+            print('Bottom left inner outer corner variation 2')
+            corner_count += 2
+
+        # Peninsulas
+        # I-shape Peninsula (Vertical)
+        case [True, True, True, True, True, False, True, True]:  # Vertical I-shape (north)
+            print('I Shape North')
+            corner_count += 2
+        case [True, False, True, True, True, True, True, True]:  # Vertical I-shape (south)
+            print('I Shape South')
+            corner_count += 2
+
+        # I-shape Peninsula (Horizontal)
+        case [True, True, True, True, True, True, True, False]:  # Horizontal I-shape (east)
+            print('I Shape East')
+            corner_count += 2
+        case [True, True, True, False, True, True, True, True]:  # Horizontal I-shape (west)
+            print('I Shape West')
+            corner_count += 2
+
+        # T-shape Peninsula (Vertical)
+        case [True, True, True, False, True, False, True, False]:  # Vertical T-shape (north)
+            print('T Shape North')
+            corner_count += 2
+        case [True, False, True, False, True, True, True, False]:  # Vertical T-shape (south)
+            print('T Shape South')
+            corner_count += 2
+
+        # T-shape Peninsula (Horizontal)
+        case [True, False, True, False, True, False, True, True]:  # Horizontal T-shape (east)
+            print('T Shape East')
+            corner_count += 2
+        case [True, False, True, True, True, False, True, False]:  # Horizontal T-shape (west)
+            print('T Shape West')
+            corner_count += 2
+
+        # L-shape Peninsula (Vertical)
+        case [True, True, True, True, False, False, True, True]:  # Vertical L-shape (north variation 1)
+            print('L Shape North variation 1')
+            corner_count += 2
+        case [True, True, True, True, True, False, False, True]:  # Vertical L-shape (north variation 2)
+            print('L Shape North variation 2')
+            corner_count += 2
+        case [False, False, True, True, True, True, True, True]:  # Vertical L-shape (south variation 1)
+            print('L Shape South variation 1')
+            corner_count += 2
+        case [True, False, False, True, True, True, True, True]:  # Vertical L-shape (south variation 2)
+            print('L Shape South variation 2')
+            corner_count += 2
+
+        # L-shape Peninsula (Horizontal)
+        case [False, True, True, True, True, True, True, False]:  # Horizontal L-shape (east variation 1)
+            print('L Shape East variation 1')
+            corner_count += 2
+        case [True, True, True, True, True, True, False, False]:  # Horizontal L-shape (east variation 2)
+            print('L Shape East variation 2')
+            corner_count += 2
+        case [True, True, False, False, True, True, True, True]:  # Horizontal L-shape (west variation 1)
+            print('L Shape West variation 1')
+            corner_count += 2
+        case [True, True, True, False, False, True, True, True]:  # Horizontal L-shape (west variation 2)
+            print('L Shape West variation 2')
+            corner_count += 2
+
+        # Edge case
+        case [False, True, True, False, False, False, True, True]:
+            print("Edge case")
+            corner_count += 2
+
+        # Cross region
+        case [True, False, True, False, True, False, True, False]: # Cross region on cardinals
+            print('Cross region variation 1')
+            corner_count += 4
+        case [False, True, False, True, False, True, False, True]: # Cross region on intercardinals
+            print('Cross region variation 2')
+            corner_count += 4
+
+        # Alone region
+        case [True, True, True, True, True, True, True, True]:
+            print('Alone region')
+            corner_count += 4
+
+    return corner_count
 class Cell():
     def __init__(self):
         self.value : str = None
@@ -36,9 +206,14 @@ class Cell():
         self.s: Cell = None
         self.w: Cell = None
         self.e: Cell = None
+
+        self.nw: Cell = None
+        self.ne: Cell = None
+        self.sw: Cell = None
+        self.se: Cell = None
         
         self.visited: bool = False
-
+        
 class Region():
     _instances = {}
 
@@ -53,17 +228,23 @@ class Region():
     def add_cell(self, cell):
         self.cells.append(cell)
 
-    def calculate_boundary(self):
-        
-        directions = ['n', 'e', 's', 'w']
-        boundary_sides = 0
-        
+    def calculate_boundaries(self):
+        boundary_sides = 0        
+        region_value = self.cells[0].value
+        directions = ['nw', 'n', 'ne', 'e', 'se', 's', 'sw', 'w']
+
         for cell in self.cells:
-            # CREATE LOGIC TO UNDERSTAND WHAT AN EDGE IS
-            pass
-                    
-        self.num_sides = boundary_sides
+            outside_region = []
+            for direction in directions:
+                new_cell: Cell = getattr(cell, direction, None)
+
+                if not new_cell or new_cell.value != region_value:
+                    outside_region.append(True)
+                else:
+                    outside_region.append(False)
+            boundary_sides += num_boundary(outside_region)
             
+        self.num_sides = boundary_sides
         return self.num_sides
 
     @classmethod
@@ -85,17 +266,33 @@ class Map():
                 cell: Cell = self._cells[i][j]
                 cell.value = self._data[i][j]
 
+                # Logic for checking NW
+                if i - 1 >= 0 and j - 1 >= 0:
+                    cell.nw = self._cells[i - 1][j - 1]
+
                 # Logic for checking N
                 if i - 1 >= 0:
                     cell.n = self._cells[i - 1][j]
+
+                # Logic for checking NE
+                if i - 1 >= 0 and j + 1 < self._num_cols:
+                    cell.ne = self._cells[i - 1][j + 1]
 
                 # Logic for checking E
                 if j + 1 < self._num_cols:
                     cell.e = self._cells[i][j + 1]
 
+                # Logic for checking SE
+                if i + 1 < self._num_rows and j + 1 < self._num_cols:
+                    cell.se = self._cells[i + 1][j + 1]
+
                 # Logic for checking S
                 if i + 1 < self._num_rows:
                     cell.s = self._cells[i + 1][j]
+
+                # Logic for checking SW
+                if i + 1 < self._num_rows and j - 1 >= 0:
+                    cell.sw = self._cells[i + 1][j - 1]
 
                 # Logic for checking W
                 if j - 1 >= 0:
@@ -176,10 +373,17 @@ def solve():
     cost_inner_outer    = 0
     cost_outer          = 0
     for key, region_list in regions.items():
+        if key == 'C':
+            pass
         for region in region_list:
+            print('------------------------------------------------')
+            print(f'Starting Region: {region.identifier}')
             cost_inner_outer    += len(region.cells) * region.perimiter_length
-            cost_outer          += len(region.cells) * region.calculate_boundary()
+            cost_outer          += len(region.cells) * region.calculate_boundaries()
+            print(f'Region: {region.identifier}. It has {region.num_sides} sides with {len(region.cells)} cells')
+            print(f'End Region: {region.identifier}. It has cost {region.num_sides*len(region.cells)}')
 
+    print('------------------------------------------------')
     print(f'Cost of fencing with inner & outer perimiter: {cost_inner_outer}\nCost of fencing with outer perimiter: {cost_outer}')
 
 if __name__ == '__main__':
